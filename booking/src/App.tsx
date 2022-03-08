3b75f365a3fed356d5df4047b4a67c9ed50b8a5b
Dionmbaye <djinodji.dionmbaye@gmail.com>
2022-03-08 21:36:57 +0100

    add navbar

--- booking/src/App.tsx
+++ booking/src/App.tsx
@@ -7,16 +7,44 @@ import CreateUser from "./components/CreateUser";
 import BookRoom from "./components/BookRoom";
 import Users from "./components/Users";
 import Room from "./components/room/Room";
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import Rooms from "./components/room/Rooms";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TypoGraphy from '@material-ui/core/Typography';
+import NavBar from "./navbar";
 
 function App() {
   return (
     <div className="App">
+      <div>
+      <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy
+              color="inherit">
+              Room Booking App
+           </TypoGraphy>
+           <NavBar/>
+          </Toolbar>
+        </AppBar>
+      </div>
       <Provider store={store}>
-        <Room />
+        {/* <Room />
         <Users />
         <CreateUser />
         <BookRoom />
-        <Bookings />
+        <Bookings /> */}
+      
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<div><h1>Home Page <br/></h1></div>} />
+            <Route path="users" element={<Users />} />
+            <Route path="rooms" element={<Room />} />
+            <Route path="bookings" element={<Bookings />} />
+          </Routes>
+         
+        </Router>
       </Provider>
     </div>
   );
