7a739a0f36325dce7f20fc5c5f3f4b1a27db014b
Dionmbaye <djinodji.dionmbaye@gmail.com>
2022-03-09 15:19:37 +0100

    push for update and create Room

--- booking/src/components/room/Rooms.tsx
+++ booking/src/components/room/Rooms.tsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { Room } from "../../domain/Room";
+import MaterialTable from '@material-table/core';
+import Button from "@material-ui/core/Button";
+import { Edit } from "@material-ui/icons";
 
 const Rooms: React.FC = () => {
     const dispatch = useDispatch();
@@ -12,6 +15,16 @@ const Rooms: React.FC = () => {
 
     const rooms = useSelector((state: RootState) => state.room.rooms);
 
+    const columns = [
+        { title: "Id", field: "id" },
+        { title: "Name", field: "name" },
+        { title: 'Actions', render: (rowData: Room) => 
+            <div>
+                <Button onClick={() => dispatch({ type: "FETCH_ROOM", payload: rowData.id })}>Edit</Button>
+            </div>
+        }
+      ];
+
     if (!rooms) {
         return <></>;
     }
@@ -20,18 +33,22 @@ const Rooms: React.FC = () => {
         height: "100px",
         width: "800px",
         margin: "5px auto",
-        border: "solid 1px"
+        // border: "solid 1px"
     };
 
     return <div style={roomStyle}>
-        {rooms.map((r: Room) =>
+        <h1>Manage Rooms</h1>
+        <Button variant="contained" color="primary" onClick={() => dispatch({ type: "OPEN_ROOM_EDITOR" })}>
+          New Room
+        </Button>
+        <br/>
+        <MaterialTable title="Rooms" columns={columns} data={rooms} />
+        {/* {rooms.map((r: Room) =>
             <li key={r.id} className="liList">
                 <button onClick={() => dispatch({ type: "FETCH_ROOM", payload: r.id })}>{r.id}</button> - {r.name}
             </li>)
-        }
-        <div>
-            <button onClick={() => dispatch({ type: "OPEN_ROOM_EDITOR" })}>New room</button>
-        </div>
+        } */}
+        
     </div>
 };
 
